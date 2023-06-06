85e1941ff791451cd873ba33f3f3b7089420d8f6
tamim-jr <internationalinf24@gmail.com>
2023-06-06 21:59:20 +0600

    add some js code of if else

--- index.js
+++ index.js
@@ -28,13 +28,48 @@
 // var dogAge = prompt("How old is your Dog?");
 // var humanAge = ((dogAge -2) *4) +21;
 // var outPut = alert("Your dog is " + humanAge + " years old in human years.");
-function getMilk(bottle) {
-    console.log("Go to Fridge");
-    console.log("Take " + bottle + " bottles of milk");    
-    console.log("and give it to me");}
-getMilk(2);
+// function getMilk(bottle) {
+//     console.log("Go to Fridge");
+//     console.log("Take " + bottle + " bottles of milk");    
+//     console.log("and give it to me");}
+// getMilk(2);
 
-function getMilk(bottle) {
-    console.log("Go to Shop and");
-    console.log("give " + (bottle * 1.5) + " USD for all bottles of milk");    console.log("and give it to me");}
-getMilk(2);
+// function getMilk(bottle) {
+//     console.log("Go to Shop and");
+//     console.log("give " + (bottle * 1.5) + " USD for all bottles of milk");    console.log("and give it to me");}
+// getMilk(2);
+
+function calculateLovePercentage() {
+    var person1 = document.getElementById("person1").value;
+    var person2 = document.getElementById("person2").value;
+    var min = 74;
+    var max = 96;
+    var lovePercentage = Math.floor(Math.random() * (max - min + 1)) + min;
+    //   var lovePercentage = Math.floor(Math.random() * 101); // Generate a random number between 0 and 100
+
+    var resultElement = document.getElementById("result");
+    resultElement.textContent = person1 + " and " + person2 + " love percentage is " + lovePercentage + "%";
+}
+
+// // <!-- // Generate a random number between 74 and 96
+// var min = 74;
+// var max = 96;
+// var randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+
+// // Output the random number
+// console.log(randomNumber); -->
+
+
+// love score by if else
+// prompt("Type your name");
+// prompt("Type your movers name");
+
+// var calLove = Math.random() *100;
+// calLove = Math.floor(calLove) +1;
+
+// if (calLove > 74){
+//     alert("Your love score " + calLove + " % is so good.");
+// }
+// else{
+//     alert("Your love score " + calLove + " %");
+// }
